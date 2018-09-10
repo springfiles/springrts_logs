0e07e40cd2a6f5641a0a42465fc00bfe476f736f
Daniel Troeder <troeder@univention.de>
2018-09-10 08:27:34 +0200

    escape/remove characters not allowed on XML 1.0 (#1)

--- springrts_logs/feeds.py
+++ springrts_logs/feeds.py
@@ -8,6 +8,8 @@
 # You should have received a copy of the GNU Affero General Public License v3
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import re
+from xml.sax.saxutils import escape as xml_escape
 from django.contrib.syndication.views import Feed
 from django.urls import reverse
 from .models import Logfile
@@ -18,14 +20,19 @@ class LatestLogfileUploadFeed(Feed):
     link = "/logfiles/"
     description = "Logfile uploads."
 
+    @staticmethod
+    def _escape(text):
+        text = xml_escape(text)
+        return re.sub(r'[\x00-\x08\x0B-\x0C\x0E-\x1F]', '', text)
+
     def items(self):
         return Logfile.objects.order_by('-created')[:20]
 
     def item_title(self, item):
-        return item.name
+        return self._escape(item.name)
 
     def item_description(self, item):
-        return item.text[:500]
+        return self._escape(item.text[:500])
 
     def item_link(self, item):
         return reverse('logfile-detail', args=[item.pk])
