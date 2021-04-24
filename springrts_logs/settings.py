ad97cc64d17d4d43b296dfdec864e7163e7415a3
abma <spring@abma.de>
2021-04-24 23:38:28 +0200

    fix AutoSchema' object has no attribute 'get_link'

--- springrts_logs/settings.py
+++ springrts_logs/settings.py
@@ -69,6 +69,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
     ],
+    'DEFAULT_SCHEMA_CLASS':'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 20,
     'DEFAULT_THROTTLE_CLASSES': (
